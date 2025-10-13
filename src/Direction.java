d66f575cbefddeb43e96d20581d2f7d30f445dd5
szabo.daniel.2022i_b <szdanika12345@gmail.com>
2025-10-13 11:30:31 +0200

    direction, item, scene

---
+++ src/Direction.java
@@ -0,0 +1,17 @@
+public enum Direction {
+    ESZAK("észak"),
+    DEL("dél"),
+    NYUGAT("nyugat"),
+    KELET("kelet");
+
+    private final String direction;
+
+    Direction(String direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public String toString() {
+        return direction;
+    }
+}
