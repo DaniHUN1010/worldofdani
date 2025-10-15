51b916644b117b130e5dac6832892c247e404466
Szabó Dániel <szdanika12345@gmail.com>
2025-10-15 13:07:21 +0200

    play and display methods, game loop with scanner

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
-        
+        Game game = new Game();
+        game.play();
+    }
+
+    private void play(){
+        Scanner scanner = new Scanner(System.in);
+        display("Üdv a várkalandban");
+
+        // Ez a játék ciklus
+        while (true){
+            String input = scanner.nextLine().toLowerCase(Locale.ROOT).trim();
+            String command = input;
+
+            switch (command){
+                case "kilep":
+                    display("Köszi a játékot!");
+                    scanner.close();
+                    return;
+            }
+        }
+    }
+
+    private void display(String message) {
+        System.out.println(message);
     }
 }
